ee99c7f4e6c0602c38dafb1ea6ee9c074a34cdcd
Joachim Tworko <joachim.tworko@deviniti.com>
2025-04-18 20:10:52 +0200

    Init

---
+++ src/main/java/com/eaglevisiongroup/eaglevision/EagleVisionApplication.java
@@ -0,0 +1,13 @@
+package com.eaglevisiongroup.eaglevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eaglevisiongroup/eaglevision/EagleVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaglevisiongroup.eaglevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
